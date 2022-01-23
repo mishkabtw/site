4a798891963c8c7dca8970371f40f7f233ca1383
Mikhail <mishabyan@gmail.com>
2022-01-23 22:43:35 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,347 @@
+"use strict"
+
+window.onload = function () {
+	const parallax = document.querySelector('.parallax');
+
+	if (parallax) {
+		const content = document.querySelector('.parallax__container');
+		const clouds = document.querySelector('.images-parallax__clouds');
+		const mountains = document.querySelector('.images-parallax__mountains');
+		const human = document.querySelector('.images-parallax__human');
+		const forClouds = 40;
+		const forMountains = 20;
+		const forHuman = 10;
+		const speed = 0.3;
+
+		let positionX = 0, positionY = 0;
+		let coordXprocent = 0, coordYprocent = 0;
+
+		function setMouseParallaxStyle() {
+			const distX = coordXprocent - positionX;
+			const distY = coordYprocent - positionY;
+
+			positionX = positionX + (distX * speed);
+			positionY = positionY + (distY * speed);
+
+			clouds.style.cssText = `transform: translate(${positionX / forClouds}%,${positionY / forClouds}%);`;
+			mountains.style.cssText = `transform: translate(${positionX / forMountains}%,${positionY / forMountains}%);`;
+			human.style.cssText = `transform: translate(${positionX / forHuman}%,${positionY / forHuman}%);`;
+
+			requestAnimationFrame(setMouseParallaxStyle);
+		}
+		setMouseParallaxStyle();
+
+		parallax.addEventListener("mousemove", function (e) {
+			const parallaxWidth = parallax.offsetWidth;
+			const parallaxHeight = parallax.offsetHeight;
+
+			const coordX = e.pageX - parallaxWidth / 2;
+			const coordY = e.pageY - parallaxHeight / 2;
+
+			coordXprocent = coordX / parallaxWidth * 100;
+			coordYprocent = coordY / parallaxHeight * 100;
+		});
+
+
+		let thresholdSets = [];
+		for (let i = 0; i <= 1.0; i += 0.005) {
+			thresholdSets.push(i);
+		}
+		const callback = function (entries, observer) {
+			const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
+			setParallaxItemsStyle(scrollTopProcent);
+		};
+		const observer = new IntersectionObserver(callback, {
+			threshold: thresholdSets
+		});
+
+		observer.observe(document.querySelector('.content'));
+
+		function setParallaxItemsStyle(scrollTopProcent) {
+			content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%);`;
+			mountains.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%);`;
+			human.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
+		}
+
+
+	}
+}
+
+
+// Инициализируем Swiper
+let myImageSlider = new Swiper('.image-slider', {
+	// Стрелки
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	// Навигация 
+	// Буллеты, текущее положение, прогрессбар
+	pagination: {
+		el: '.swiper-pagination',
+		/*
+		// Буллеты
+		type: 'bullets',
+		clickable: true,
+		// Динамические буллеты
+		dynamicBullets: true,
+		// Кастомные буллеты
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + (index + 1) + '</span>';
+		},
+		*/
+		/*
+		// Фракция
+		type: 'fraction',
+		// Кастомный вывод фракции
+		renderFraction: function (currentClass, totalClass) {
+			return 'Фото <span class="' + currentClass + '"></span>' +
+				' из ' +
+				'<span class="' + totalClass + '"></span>';
+		},
+		*/
+		// Прогрессбар
+		//type: 'progressbar'
+	},
+	// Скролл
+	/*
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		// Возможность перетаскивать скролл
+		draggable: true
+	},
+	*/
+
+	// Включение/отключение
+	// перетаскивания на ПК
+	simulateTouch: true,
+	// Чувствительность свайпа
+	touchRatio: 1,
+	// Угол срабатывания свайпа/перетаскивания
+	touchAngle: 45,
+	// Курсор перетаскивания
+	grabCursor: true,
+
+	// Переключение при клике на слайд
+	slideToClickedSlide: false,
+
+	// Навигация по хешу
+	hashNavigation: {
+		// Отслеживать состояние
+		watchState: true,
+	},
+
+	// Управление клавиатурой
+	keyboard: {
+		// Включить\выключить
+		enabled: true,
+		// Включить\выключить
+		// только когда слайдер
+		// в пределах вьюпорта
+		onlyInViewport: true,
+		// Включить\выключить
+		// управление клавишами
+		// pageUp, pageDown
+		pageUpDown: true,
+	},
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 3,
+
+	// Отключение функционала
+	// если слайдов меньше чем нужно
+	watchOverflow: true,
+
+	// Отступ между слайдами
+	spaceBetween: 20,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 1,
+
+	// Активный слайд по центру
+	centeredSlides: false,
+
+	// Стартовый слайд.
+	initialSlide: 0,
+
+	// Мультирядность
+	slidesPerColumn: 1,
+
+	// Бесконечный слайдер
+	loop: true,
+
+	// Кол-во дублирующих слайдов
+	loopedSlides: 0,
+
+	// Свободный режим
+	freeMode: true,
+
+	// Автопрокрутка
+	/*
+	autoplay: {
+		// Пауза между прокруткой
+		delay: 1000,
+		// Закончить на последнем слайде
+		stopOnLastSlide: true,
+		// Отключить после ручного переключения
+		disableOnInteraction: false
+	},
+	*/
+
+	// Скорость
+	speed: 350,
+
+	// Вертикальный слайдер
+	direction: 'horizontal',
+
+
+	// Эффекты переключения слайдов.
+	// Листание
+	effect: 'slide',
+
+	/*
+	// Эффекты переключения слайдов.
+	// Cмена прозрачности
+	effect: 'fade',
+
+	// Дополнение к fade
+	fadeEffect: {
+		// Параллельная
+		// смена прозрачности
+		crossFade: true
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Переворот
+	effect: 'flip',
+
+	// Дополнение к flip
+	flipEffect: {
+		// Тень
+		slideShadows: true,
+		// Показ только активного слайда
+		limitRotation: true
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Куб
+	effect: 'cube',
+
+	// Дополнение к cube
+	cubeEffect: {
+		// Настройки тени
+		slideShadows: true,
+		shadow: true,
+		shadowOffset: 20,
+		shadowScale: 0.94
+	},
+	*/
+	/*
+	// Эффекты переключения слайдов.
+	// Эффект потока
+	effect: 'coverflow',
+
+	// Дополнение к coverflow
+	coverflowEffect: {
+		// Угол
+		rotate: 20,
+		// Наложение
+		stretch: 50,
+		// Тень
+		slideShadows: true,
+	},
+	*/
+	/*
+	// Брейк поинты (адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		480: {
+			slidesPerView: 2,
+		},
+		992: {
+			slidesPerView: 3,
+		}
+	},
+	*/
+	/*
+	// Брейк поинты (адаптив)
+	// Соотношение сторон
+	breakpoints: {
+		'@0.75': {
+			slidesPerView: 1,
+		},
+		'@1.00': {
+			slidesPerView: 2,
+		},
+		'@1.50': {
+			slidesPerView: 3,
+		}
+	},
+	*/
+
+	// Отключить предзагрузка картинок
+	preloadImages: false,
+	// Lazy Loading
+	// (подгрузка картинок)
+	lazy: {
+		// Подгружать на старте
+		// переключения слайда
+		loadOnTransitionStart: false,
+		// Подгрузить предыдущую
+		// и следующую картинки
+		loadPrevNext: false,
+	},
+	// Слежка за видимыми слайдами
+	watchSlidesProgress: true,
+	// Добавление класса видимым слайдам
+	watchSlidesVisibility: true,
+
+	// Зум картинки
+	zoom: {
+		// Макимальное увеличение
+		maxRatio: 5,
+		// Минимальное увеличение
+		minRatio: 1,
+	},
+
+	// Миниатюры (превью)
+	/*
+	thumbs: {
+		// Свайпер с мениатюрами
+		// и его настройки
+		swiper: {
+			el: '.image-mini-slider',
+			slidesPerView: 5,
+		}
+	},
+	*/
+	/*
+	// Передача управления
+	controller: {
+		control: myTextSlider
+	},
+	*/
+
+	// Обновить свайпер
+	// при изменении элементов слайдера
+	observer: true,
+
+	// Обновить свайпер
+	// при изменении родительских
+	// элементов слайдера
+	observeParents: true,
+
+	// Обновить свайпер
+	// при изменении дочерних
+	// элементов слайда
+	observeSlideChildren: true,
+});
+
